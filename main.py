82a5c634380c788ff50f69cdf9d41783387ca51a
anuragtimesinternet1 <anurag.mishra1@timesinternet.in>
2026-09-21 09:34:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ import csv
 import json
 from googleapiclient.discovery import build
 from google.oauth2.service_account import Credentials
+from oauth2client.service_account import ServiceAccountCredentials
 GOOGLE_CREDENTIALS_JSON = os.getenv('GOOGLE_CREDENTIALS_JSON')
 print("NETWORK_CODE :", os.getenv('NETWORK_CODE'))
 print("APPLICATION_NAME:", os.getenv('APPLICATION_NAME'))
@@ -20,6 +21,8 @@ print("CLIENT_ID:", os.getenv('CLIENT_ID'))
 print("CLIENT_SECRET:", os.getenv('CLIENT_SECRET'))
 print("REFRESH_TOKEN:", os.getenv('REFRESH_TOKEN'))
 from impressions_clicks_of_order import download_combined_report_by_name
+from Update_predicted_adunits_sheet import update_predicted_units_in_sheet
+OUTPUT_COLUMN = 'AK' 
 
 sheet_url = "https://docs.google.com/spreadsheets/d/1u68QXESgLIlfDHzSY_9QVCd9pFIk-sCqiSBB9IERz9g/edit?gid=1080823886#gid=1080823886"
 sheet_id=1080823886
@@ -39,8 +42,14 @@ if __name__ == '__main__':
 
     if report_file:
         update_sheet_from_report(client, sheet_url, sheet_id, report_file)
-
-
+        update_predicted_units_in_sheet(
+        client,
+        sheet_url,
+        sheet_id,
+        GOOGLE_CREDENTIALS_JSON,
+        skip_header=True,
+        output_column=OUTPUT_COLUMN
+    )
          
             
     
